316ff89d056ea9096c325a46e0eb57a3e7027f96
bigsaso <sal.logozzo@gmail.com>
2023-12-04 20:21:54 -0500

    Checking every 3rd frame

--- cps843_project.py
+++ cps843_project.py
@@ -99,7 +99,7 @@ def process_image(input, output):
     # Release only for video
     cv2.destroyAllWindows()
 
-def process_video(input, output, playback_speed=1.0):
+def process_video(input, output):
     # Colors
     red = (255, 0, 0)
     green = (0, 255, 0)
@@ -123,6 +123,10 @@ def process_video(input, output, playback_speed=1.0):
     paused = False
     current_frame = 0
 
+    # Store last boxes and num_people
+    last_boxes = []
+    last_num_people = 0
+
     while cap.isOpened():
         # Read a frame from the video
         ret, frame = cap.read()
@@ -134,59 +138,75 @@ def process_video(input, output, playback_speed=1.0):
             # Initialize num_people for each frame
             num_people = 0
 
-            # Preprocessing
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gaussian_blur = cv2.GaussianBlur(gray, (9, 9), 0)
+            if current_frame % 3 == 0:
+                # Reset last boxes and number of people
+                last_boxes = []
+                last_num_people = 0
 
-            # Object Detection (YOLO)
-            yolo = cv2.dnn.readNet('yolov3.weights', 'yolov3.cfg')
-            layer_names = yolo.getLayerNames()
-            output_layers = [layer_names[i - 1] for i in yolo.getUnconnectedOutLayers().flatten()]
-            blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False) # can be changed to (608, 608), this increases input size of object.
-            yolo.setInput(blob)
-            outs = yolo.forward(output_layers)
+                # Preprocessing
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                gaussian_blur = cv2.GaussianBlur(gray, (9, 9), 0)
 
-            # Bounding Boxes
-            class_ids = []
-            confidences = []
-            boxes = []
-            for out in outs:
-                for detection in out:
-                    scores = detection[5:]
-                    class_id = np.argmax(scores)
-                    confidence = scores[class_id]
-                    if confidence > 0.3 and class_id == 0:  # Check if the detected class is a person
-                        num_people += 1
-                        center_x = int(detection[0] * width)
-                        center_y = int(detection[1] * height)
-                        w = int(detection[2] * width)
-                        h = int(detection[3] * height)
+                # Object Detection (YOLO)
+                yolo = cv2.dnn.readNet('yolov3.weights', 'yolov3.cfg')
+                layer_names = yolo.getLayerNames()
+                output_layers = [layer_names[i - 1] for i in yolo.getUnconnectedOutLayers().flatten()]
+                blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False) # can be changed to (608, 608), this increases input size of object.
+                yolo.setInput(blob)
+                outs = yolo.forward(output_layers)
 
-                        # Rectangle coordinates
-                        x = int(center_x - w / 2)
-                        y = int(center_y - h / 2)
+                # Bounding Boxes
+                class_ids = []
+                confidences = []
+                boxes = []
+                for out in outs:
+                    for detection in out:
+                        scores = detection[5:]
+                        class_id = np.argmax(scores)
+                        confidence = scores[class_id]
+                        if confidence > 0.3 and class_id == 0:  # Check if the detected class is a person
+                            num_people += 1
+                            center_x = int(detection[0] * width)
+                            center_y = int(detection[1] * height)
+                            w = int(detection[2] * width)
+                            h = int(detection[3] * height)
 
-                        boxes.append([x, y, w, h])
-                        confidences.append(float(confidence))
-                        class_ids.append(class_id)
+                            # Rectangle coordinates
+                            x = int(center_x - w / 2)
+                            y = int(center_y - h / 2)
 
-            # Apply Non-max suppression
-            if boxes and confidences:
-                indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.3, 0.4)
+                            boxes.append([x, y, w, h])
+                            confidences.append(float(confidence))
+                            class_ids.append(class_id)
 
-                # Check if indices is a tuple, and convert it to a NumPy array
-                indices = np.array(indices) if isinstance(indices, tuple) else indices
-            
-                # Draw bounding boxes for YOLO detections
-                num_people = len(indices)
-                for i in indices.flatten():
-                    box = boxes[i]
-                    x, y, w, h = box[0], box[1], box[2], box[3]
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), green, 2)
-                    
+                # Apply Non-max suppression
+                if boxes and confidences:
+                    indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.3, 0.4)
+
+                    # Check if indices is a tuple, and convert it to a NumPy array
+                    indices = np.array(indices) if isinstance(indices, tuple) else indices
+                
+                    # Draw bounding boxes for YOLO detections
+                    num_people = len(indices)
+                    for i in indices.flatten():
+                        # last_boxes.append(boxes[i])
+                        box = boxes[i]
+                        x, y, w, h = box[0], box[1], box[2], box[3]
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), green, 2)
+                        
+                # # # Display Count
+                # cv2.putText(frame, 'Status : Detecting ', (40, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, red, 2)
+                # cv2.putText(frame, f'Total People Detected : {num_people}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, red, 2)
+
+                # last_boxes = boxes
+                last_num_people = num_people
+
+            # for box in last_boxes:
+            #     x, y, w, h = box[0], box[1], box[2], box[3]
+            #     cv2.rectangle(frame, (x, y), (x + w, y + h), green, 2)
             # Display Count
-            cv2.putText(frame, 'Status : Detecting ', (40, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, red, 2)
-            cv2.putText(frame, f'Total People Detected : {num_people}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, red, 2)
+            cv2.putText(frame, 'Status : Not Detecting ', (40, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, red, 2)
+            cv2.putText(frame, f'Total People Detected : {last_num_people}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, red, 2)
 
             # Show the frame
             cv2.imshow('Frame', frame)
