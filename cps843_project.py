6658e639114d1b4c796853fcbc1188fa78bab78f
AnnazG <annasghani2020@gmail.com>
2023-12-04 19:18:58 -0500

    Added video processing, video playback rate needs to be fixed

--- cps843_project.py
+++ cps843_project.py
@@ -100,7 +100,127 @@ def process_image(input, output):
     cv2.destroyAllWindows()
 
 def process_video(input, output):
-    print(f"Processing video from {input} and saving result to {output}")
+    # Colors
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+
+    # Load video
+    cap = cv2.VideoCapture(input, cv2.CAP_MSMF)
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    # Check for errors
+    if not cap.isOpened():
+        print("Error opening video stream or file")
+        return
+    
+    # Define the codec and create a VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # You can also try other codecs like 'XVID'
+    out = cv2.VideoWriter(output, fourcc, 20.0, (width, height))
+
+    # Variables for controlling video playback
+    paused = False
+    current_frame = 0
+
+    while cap.isOpened():
+        # Read a frame from the video
+        ret, frame = cap.read()
+
+        if not paused:
+            current_frame += 1
+
+        if ret:
+            # Initialize num_people for each frame
+            num_people = 0
+
+            # Preprocessing
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gaussian_blur = cv2.GaussianBlur(gray, (9, 9), 0)
+
+            # Object Detection (YOLO)
+            yolo = cv2.dnn.readNet('yolov3.weights', 'yolov3.cfg')
+            layer_names = yolo.getLayerNames()
+            output_layers = [layer_names[i - 1] for i in yolo.getUnconnectedOutLayers().flatten()]
+            blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False) # can be changed to (608, 608), this increases input size of object.
+            yolo.setInput(blob)
+            outs = yolo.forward(output_layers)
+
+            # Bounding Boxes
+            class_ids = []
+            confidences = []
+            boxes = []
+            for out in outs:
+                for detection in out:
+                    scores = detection[5:]
+                    class_id = np.argmax(scores)
+                    confidence = scores[class_id]
+                    if confidence > 0.3 and class_id == 0:  # Check if the detected class is a person
+                        num_people += 1
+                        center_x = int(detection[0] * width)
+                        center_y = int(detection[1] * height)
+                        w = int(detection[2] * width)
+                        h = int(detection[3] * height)
+
+                        # Rectangle coordinates
+                        x = int(center_x - w / 2)
+                        y = int(center_y - h / 2)
+
+                        boxes.append([x, y, w, h])
+                        confidences.append(float(confidence))
+                        class_ids.append(class_id)
+
+            # Apply Non-max suppression
+            if boxes and confidences:
+                indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.3, 0.4)
+
+                # Check if indices is a tuple, and convert it to a NumPy array
+                indices = np.array(indices) if isinstance(indices, tuple) else indices
+            
+                # Draw bounding boxes for YOLO detections
+                num_people = len(indices)
+                for i in indices.flatten():
+                    box = boxes[i]
+                    x, y, w, h = box[0], box[1], box[2], box[3]
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), green, 2)
+                    
+            # Display Count
+            cv2.putText(frame, 'Status : Detecting ', (40, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, red, 2)
+            cv2.putText(frame, f'Total People Detected : {num_people}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, red, 2)
+
+            # Show the frame
+            cv2.imshow('Frame', frame)
+
+            # Write the frame to the output video file
+            out.write(frame)
+
+            # Press P to pause/resume the video
+            key = cv2.waitKey(60)  # Decreased wait time
+            if key == ord('p'):
+                paused = not paused
+
+            # Press Q on the keyboard to exit
+            elif key == ord('q'):
+                break
+
+            # Press S to skip frames (10 frames per press)
+            elif key == ord('s'):
+                current_frame += 10
+
+            # Set the video capture to the specified frame
+            cap.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
+
+        else:
+            break
+        
+    # Release the VideoWriter object
+    out.release()
+
+    # Release the video capture object and close all windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+    print(f"Total People Detected in the video: {num_people}")
 
 def main():
     # Create an ArgumentParser object
